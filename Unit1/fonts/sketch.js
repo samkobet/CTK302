1611cdff02356dd512093a4429d5390a16bfb91d
samkobet <89150689+samkobet@users.noreply.github.com>
2022-02-12 00:25:47 -0600

    Add files via upload

---
+++ Unit1/fonts/sketch.js
@@ -0,0 +1,20 @@
+let font1, font2 ;
+
+function setup() {
+  createCanvas(1000, 500);
+  font1 = loadFont("assets/Doctor Glitch.otf") ;
+  font2 = loadFont("assets/Game of Squids.ttf") ;
+}
+
+function draw() {
+  background(200) ;
+
+  textSize(40);
+  textFont(font1) ;
+  text("God gives his toughest battles", 100, 100);
+
+textSize(50);
+  textFont(font2) ;
+  text("to his silliest clowns", 100, 200)
+
+}
