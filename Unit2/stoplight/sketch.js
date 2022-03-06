f9738ed30cd2e1a25a9b67fe10f79f892811aada
samkobet <89150689+samkobet@users.noreply.github.com>
2022-03-05 19:07:57 -0600

    Add files via upload

---
+++ Unit2/stoplight/sketch.js
@@ -0,0 +1,74 @@
+let state = 0;
+let timer = 0;
+let x = 0;
+let velocity = 10;
+
+function setup() {
+  createCanvas(800, 800);
+  ellipseMode(CENTER);
+  rectMode(CENTER);
+}
+
+function draw() {
+
+  background(100);
+  fill('#414141');
+  rect(width / 2, height / 2, 200, 600);
+
+  //car code
+  fill('blue');
+  rect(x, 750, 75, 20);
+  x = x + velocity;
+  if (x > width) {
+    x = 0;
+  }
+
+  switch (state) {
+    case 0: //green
+      text("1", 100, 100);
+      fill("gray");
+      ellipse(width / 2, height / 2 - 170, 150, 150);
+      fill("gray");
+      ellipse(width / 2, height / 2, 150, 150);
+      fill("green");
+      ellipse(width / 2, height / 2 + 170, 150, 150);
+      velocity = 10;
+      break;
+
+    case 1: //yellow
+      text("2", 100, 100);
+      fill("gray");
+      ellipse(width / 2, height / 2 - 170, 150, 150);
+      fill("yellow");
+      ellipse(width / 2, height / 2, 150, 150);
+      fill("gray");
+      ellipse(width / 2, height / 2 + 170, 150, 150);
+      velocity = 5;
+      break;
+    case 2: //red
+      text("0", 100, 100);
+      fill("red");
+      ellipse(width / 2, height / 2 - 170, 150, 150);
+      fill("gray");
+      ellipse(width / 2, height / 2, 150, 150);
+      fill("gray");
+      ellipse(width / 2, height / 2 + 170, 150, 150);
+      velocity = 0;
+      break;
+
+
+  }
+
+  timer = timer + 1;
+  if (timer > 100) {
+    timer = 0;
+    state++;
+    if (state > 2) state = 0;
+  }
+
+}
+
+function mouseReleased() {
+  state++;
+  if (state > 2) state = 0;
+}
